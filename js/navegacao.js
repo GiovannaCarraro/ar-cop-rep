0d79bdf7981e3c87d0ff5c2b2d8c1eab8f87427d
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-02-27 09:00:51 -0300

    fazendo o css

---
+++ js/navegacao.js
@@ -0,0 +1,15 @@
+function criarBotaoVoltar(){
+// 1. Criar o elemento botão
+const btnVoltar = document.createElement('button');
+
+btnVoltar.innerText = 'Voltar';
+btnVoltar.style.marginTop = '20px';
+
+btnVoltar.addEventListener('click', function(){
+    window.location.href = '../index.html'
+});
+
+document.body.appendChild(btnVoltar)
+};
+
+window.onload = criarBotaoVoltar()
