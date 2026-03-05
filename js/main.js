cb4552887752aef55ab1d64aca567f4f2d2a47b3
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-03-05 11:30:22 -0300

    fazer o css funcionar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const btnP1 = document.querySelector('#inputP1');
 const btnP2 = document.querySelector('#inputP2');
 const btnP3 = document.querySelector('#inputP3');
+const btnP4 = document.querySelector('#inputP4');
 
 // método addEventListener c função anonima
 
@@ -14,6 +15,10 @@ btnP2.addEventListener('click', function(){
 
 btnP3.addEventListener('click', function(){
     window.location.href='./pages/projeto03.html'
+});
+
+btnP4.addEventListener('click', function(){
+    window.location.href='./pages/projeto04.html'
 });
 
 
